663ca40a6ea494857f30997d81d33776b202eeb3
one2joe <one2joe@gmail.com>
2026-07-02 10:44:49 +0700

    clean: remove all source files, keep only index.html with test2

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-
-export default defineConfig({
-  site: 'https://party-villa-balloons.pages.dev',
-  output: 'static',
-  integrations: [sitemap()],
-});
